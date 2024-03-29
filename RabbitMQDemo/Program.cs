b38e0669c25bf06d21bbb6f5c11ff0fa19083d19
AnvaySingh <anvaysingh05@gmail.com>
2024-03-30 00:40:35 +0530

    Initial Commit

---
+++ RabbitMQDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+// Provide a Uri string below. Here guest:guest is the username and password respectively,
+// 5672 is the port for sending queue messages to the server, defined in docker container
+factory.Uri =new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App"; //Any name that you provide
+
+IConnection cnn = factory.CreateConnection();
+
+// IModel is the interface of RabbitMQ to create a model
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName= "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName,false,false,false,null);
+channel.QueueBind(queueName,exchangeName,routingKey,null);
+
+// When you send a message through a queue, it will just send as byte
+// We have a message encoded as a byte array to be sent
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello Anvay");
+
+//Use the channel to publish in the exchange
+channel.BasicPublish(exchangeName,routingKey,null,messageBodyBytes);
+
+channel.Close();
+cnn.Close();
---
+++ RabbitReceiver1/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+// Provide a Uri string below. Here guest:guest is the username and password respectively,
+// 5672 is the port for sending queue messages to the server, defined in docker container
+factory.Uri =new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Receiver1 App"; //Any name that you provide
+
+//Connection
+IConnection cnn = factory.CreateConnection();
+
+// IModel is the interface of RabbitMQ to create a model
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName= "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName,false,false,false,null);
+channel.QueueBind(queueName,exchangeName,routingKey,null);
+
+//prefetchSize:0 - means not limiting the size of the message
+//prefetchCount:1 - 1 message to be sent at once. Defining the number of messages to process at a time.
+//globa:false - means to apply this setting to the current instance only
+channel.BasicQos(0,1,false);
+
+//Consumer setup
+var consumer=new EventingBasicConsumer(channel);
+consumer.Received +=(sender, args) => 
+{
+    var body= args.Body.ToArray();
+
+    //Decode the message back to string which was passed earlier in RabbitMQDemo
+    string message=Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Message Received: {message}");
+
+    // Acknowledgement of the message delivered
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+//Tag of the overall consume system
+string ConsumerTag =  channel.BasicConsume(queueName,false,consumer);
+
+Console.ReadLine();
+
+// Handle the consumer system and close it
+channel.BasicCancel(ConsumerTag);
+
+channel.Close();
+cnn.Close();
